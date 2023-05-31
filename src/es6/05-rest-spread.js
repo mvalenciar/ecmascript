4d5e5b90a309dd71330825fc5a5346141c1abac1
Milton Valencia R <miltonalonsovalenciarincon@gmail.com>
2023-05-31 18:38:40 -0500

    spread operator y rest

--- src/es6/05-rest-spread.js
+++ src/es6/05-rest-spread.js
@@ -12,3 +12,24 @@ const user = {
 };
 const { name, age } = user;
 console.log(name, age);
+
+// spread operator
+
+const person = {
+  name: 'Milton',
+  age: 30,
+};
+
+const country = 'colombia';
+
+const data = { ...person, country };
+console.log(data);
+
+// rest
+
+function sum(num, ...values) {
+  console.log(values);
+  console.log(num + values[0]);
+}
+
+sum(1, 2, 3);
