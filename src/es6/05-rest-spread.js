9b6d9fb9525b5ddcf42bbcc70302c461405e5f3b
Milton Valencia R <miltonalonsovalenciarincon@gmail.com>
2023-05-31 18:05:27 -0500

    destructuración de arrays y objetos

---
+++ src/es6/05-rest-spread.js
@@ -0,0 +1,14 @@
+// arrays destructuring
+
+const fruits = ['Apple', 'Banana'];
+const [a, b] = fruits;
+console.log(a, b);
+
+// object destructuring
+
+const user = {
+  name: 'Milton',
+  age: '30',
+};
+const { name, age } = user;
+console.log(name, age);
