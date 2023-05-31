3d41543b2c7033186847af919124b2e33684b437
Milton Valencia R <miltonalonsovalenciarincon@gmail.com>
2023-05-31 17:45:52 -0500

    practica string es6

---
+++ src/es6/02-strings.js
@@ -0,0 +1,18 @@
+const hello = 'Hello';
+const world = 'World';
+
+const epicPhrase = hello + ' ' + world;
+console.log(epicPhrase);
+
+// template literals
+const templateLiterals = `${hello} ${world}`;
+console.log(templateLiterals);
+
+// Multi-line strings
+
+const lorem = 'Esto es un string\n' + 'Esto es otra linea';
+console.log(lorem);
+
+const lorem2 = `Esto es un estrin
+continuación del string`;
+console.log(lorem2);
