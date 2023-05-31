8ee5d18c053910c197e70dd49e427a2974a5ab2b
Milton Valencia R <miltonalonsovalenciarincon@gmail.com>
2023-05-31 18:51:59 -0500

    objetos literales

---
+++ src/es6/06-object-literals.js
@@ -0,0 +1,14 @@
+function newUser(user, age, country, id) {
+  return {
+    // user: user,
+    // age: age,
+    // country: country,
+    // id: id,
+    user,
+    age,
+    country,
+    id,
+  };
+}
+
+console.log(newUser('juan', 15, 'Colombia', 1));
